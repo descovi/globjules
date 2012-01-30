bc9b5a83c91293435cfa24f320cdf1bc79ce503d
Alessandro <descovi@gmail.com>
2012-01-30 22:15:46 +0100

    Reset al cambio di foto.

--- js/audio-background.js
+++ js/audio-background.js
@@ -1,6 +1,8 @@
-var mp3 = document.createElement("audio");
-var path = './materiale/audio/GURGLE.wav';
-mp3.setAttribute('src', path);
-mp3.load();
-mp3.volume = 0.2
-mp3.play()
+//document.addEventListener('click',function(){
+	var mp3 = document.createElement("audio");
+	var path = './materiale/audio/GURGLE.wav';
+	mp3.setAttribute('src', path);
+	mp3.load();
+	mp3.volume = 0.2
+	mp3.play()
+
--- js/colonna_controls.js
+++ js/colonna_controls.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	// NASCONDI VOCABOLARIO //
 	$('#vocabolario').bind('click',function(){$(this).hide();})
 
-dati_da_guardare = '#dati-'+currentLetter;
+	dati_da_guardare = '#dati-'+currentLetter;
 	index_totali = $(dati_da_guardare+' ol').length;
 
 	//	MOSTRA NASCONDI FRECCIA
@@ -31,6 +31,8 @@ function verifica_frecce(){
 // CHANGE DATI
 function change_dati(giu_o_su){
 	var percorso_base = $('#player').first().attr('src');
+
+	//1. prima del cambio di index
 	var parole_da_cambiare = $(dati_da_guardare+' ol').get(index_attuale).innerHTML;
 
 	if (giu_o_su == 1 ){
@@ -46,24 +48,29 @@ function change_dati(giu_o_su){
 	}
 	verifica_frecce();
 
+	//2. dopo il cambio di index
 	var parole_da_inserire = $(dati_da_guardare+' ol').get(index_attuale).innerHTML;
 
 
 	//AUDIO PLAY 
 
 	var nuovo_percorso = percorso_base.replace(parole_da_cambiare,parole_da_inserire)
+	var nuovo_percorso = nuovo_percorso.replace('2','1');
 
 	var pl = $('#player').get(0)
 	$(pl).attr('src', nuovo_percorso);
+
 	pl.load();
 	pl.play();
-		console.log('->'+parole_da_inserire)
 
-	var vecchiaImg = $('#animazione').css('background-image');
-	var nuovaImg = vecchiaImg.replace(parole_da_cambiare,parole_da_inserire)
+	var vecchia_img = $('#animazione').css('background-image');
+	var nuova_img = vecchia_img.replace(parole_da_cambiare,parole_da_inserire)
 
-	$('#animazione').css('background-image',nuovaImg)
+	$('#animazione').css('background-image',nuova_img)
+	
+	//reset posizi
+	$('#animazione').css('background-position-y','0px')
 
-	console.log($('#animazione').css('background-image'));
+
 
 }
